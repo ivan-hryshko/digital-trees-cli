5e7021f269c2728c38ac7b1bd0e353ca6437c775
Ivan Hryshko <ivan.hryshko@webbylab.com>
2025-08-04 21:36:59 +0300

    feat: add basic project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('hello ');
+const args = process.argv.slice(2);
+console.log('args :>> ', args);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+console.log('hello ');
+
+const args = process.argv.slice(2);
+console.log('args :>> ', args);
